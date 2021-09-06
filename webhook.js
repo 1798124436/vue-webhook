1e512f0613a54e948f7e2aba910163fe3ef57909
wxm <1798124436@qq.com>
2021-09-06 16:08:35 +0800

    1

--- webhook.js
+++ webhook.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto');
 let http = require('http');
 let SECRET = '123456'
+let {spawn} = require('child_process')
 
 function sign(body){
   return `sha1=`+ crypto.createHmac('sha1',SECRET).update(body).disgest('hex')
@@ -21,9 +22,23 @@ let server = http.createServer((req,res)=>{
       if(signature !== sign(body)){
         return res.end('Not Allowed')
       }
+      res.setHeader('Content-Type','application/json');
+      res.end(JSON.stringify({ok:true}));
+      if(event == 'push'){
+        let payload =JSON.parse(body)
+        let child = spawn('sh',[`./${payload.reposity.name}.sh`])
+        let buffers = [];
+        child.stdout.on('data',(bufffer)=>{
+          buffers.push(buffer);
+        })
+        child.stdout.on('end',(bufffer)=>{
+          let log = Buffer.concat(buffers);
+          console.log(log)
+
+        })
+      }
     })
-    res.setHeader('Content-Type','application/json');
-    res.end(JSON.stringify({ok:true}));
+    
   }else{  
     res.end('404')
   }
