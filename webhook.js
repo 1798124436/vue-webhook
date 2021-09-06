ba94b6533263ff4be2d690084113f6fd7342bf4a
wxm <1798124436@qq.com>
2021-09-06 14:19:30 +0800

    1

--- webhook.js
+++ webhook.js
@@ -1,7 +1,27 @@
+const crypto = require('crypto');
 let http = require('http');
+let SECRET = '123456'
+
+function sign(body){
+  return `sha1=`+ crypto.createHmac('sha1',SECRET).update(body).disgest('hex')
+}
+
 let server = http.createServer((req,res)=>{
   console.log(req.method,req.url)
   if(req.method == "POST" && req.url == '/webhook'){
+    let buffers = [];
+    req.on('data',(buffer)=>{
+      buffers.push(buffer)
+    })
+    res.on('end',(buffer)=>{
+      let body = Buffer.concat(buffers)
+      let event = req.header['x-gitHub-event'] //event=push
+      //github 请求过来的时候，要传递请求体body另外还会传一个签名，需要验证签名
+      let signature = req.headers['x-hub-Signature']
+      if(signature !== sign(body)){
+        return res.end('Not Allowed')
+      }
+    })
     res.setHeader('Content-Type','application/json');
     res.end(JSON.stringify({ok:true}));
   }else{  
